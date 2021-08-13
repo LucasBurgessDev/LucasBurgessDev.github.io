eb7985914ad699c71db726d26ac32cead531b7b7
LucasBurgessDev <lucasburgess8@gmail.com>
2021-08-13 13:36:48 +0100

    Resume update

--- lucasburgessdevportfolioblog/src/components/pages/Resume.js
+++ lucasburgessdevportfolioblog/src/components/pages/Resume.js
@@ -1,57 +1,10 @@
 import '../../App.css';
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import pdfFile from "../../docs/Burgess_Luke_Data_Architect.pdf";
+import React from 'react';
+import ResumeItems from '../ResumeItems';
 
 function Resume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
-  function changePage(offset) {
-    setPageNumber(prevPageNumber => prevPageNumber + offset);
-  }
-
-  function previousPage() {
-    changePage(-1);
-  }
-
-  function nextPage() {
-    changePage(1);
-  }
-
   return (
-    <>
-      <Document
-        file={pdfFile}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <div>
-        <p>
-          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-        </p>
-        <button
-          type="button"
-          disabled={pageNumber <= 1}
-          onClick={previousPage}
-        >
-          Previous
-        </button>
-        <button
-          type="button"
-          disabled={pageNumber >= numPages}
-          onClick={nextPage}
-        >
-          Next
-        </button>
-      </div>
-    </>
+    <ResumeItems />
   );
 }
 
