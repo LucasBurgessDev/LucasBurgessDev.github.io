a11983ec3ae8f8c678cbf56580bc183c549c176d
LucasBurgessDev <lucasburgess8@gmail.com>
2021-08-18 22:14:50 +0100

    update redundant code

--- src/components/ResumeItems.js
+++ src/components/ResumeItems.js
@@ -3,14 +3,14 @@ import './Resume.css';
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import pdfFile from "../docs/Burgess_Luke_Data_Architect.pdf";
-import Video from '../videos/video-5.mp4';
+//import Video from '../videos/video-5.mp4';
 
 function ResumeItems() {
     const [numPages, setNumPages] = useState(null);
 
-    function onDocumentLoadSuccess({ numPages }) {
+/*     function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
-    }
+    } */
 return (
         <div className='resume'>
 {/*             <video src={Video} alt="background - Video by Pressmaster from Pexels" autoPlay loop muted />*/}
@@ -18,13 +18,13 @@ return (
             <div className="resume-pages">
                         <Document
                             file={pdfFile}
-                            onLoadSuccess={onDocumentLoadSuccess}
+                            //onLoadSuccess={onDocumentLoadSuccess}
                         >
                             <Page pageNumber={1} />
                         </Document>
                         <Document
                             file={pdfFile}
-                            onLoadSuccess={onDocumentLoadSuccess}
+                            //onLoadSuccess={onDocumentLoadSuccess}
                         >
                             <Page pageNumber={2} />
                         </Document>
