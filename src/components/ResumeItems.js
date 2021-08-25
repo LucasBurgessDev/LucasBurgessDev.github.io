2a16d8f08a708faac521dd86fe3ef15438919268
LucasBurgessDev <lucasburgess8@gmail.com>
2021-08-25 17:04:16 +0100

    pdf render changes

--- src/components/ResumeItems.js
+++ src/components/ResumeItems.js
@@ -1,29 +1,23 @@
 import '../App.css';
 import './Resume.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { Document, Page } from 'react-pdf/dist/umd/entry.webpack';
 import pdfFile from "../docs/Burgess_Luke_Data_Architect.pdf";
 //import Video from '../videos/video-5.mp4';
 
 function ResumeItems() {
-    const [numPages, setNumPages] = useState(null);
 
-     function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    }
 return (
         <div className='resume'>
 {/*             <video src={Video} alt="background - Video by Pressmaster from Pexels" autoPlay loop muted />*/}
             <div className="resume-pages">
-                        <Document
+                         <Document
                             file={pdfFile}
-                            onLoadSuccess={onDocumentLoadSuccess}
                         >
                             <Page pageNumber={1} />
                         </Document>
                         <Document
                             file={pdfFile}
-                            onLoadSuccess={onDocumentLoadSuccess}
                         >
                             <Page pageNumber={2} />
                         </Document>
