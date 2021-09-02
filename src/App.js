09812060ae7418d828b132ff848e8c3b7df42c31
LucasBurgessDev <lucasburgess8@gmail.com>
2021-09-02 17:16:36 +0100

    added google analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Home from "./components/pages/Home";
 import Projects from "./components/pages/Projects";
 import Resume from "./components/pages/Resume";
 import ContactMe from "./components/pages/ContactMe";
+import ReactGA from 'react-ga';
+import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = "G-44YREV2G8B"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+})
 
 function App() {
   return (
