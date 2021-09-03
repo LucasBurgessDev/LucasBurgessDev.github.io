87b4e1b3b68c70675ce46daf8c9d8f5dc596255e
LucasBurgessDev <lucasburgess8@gmail.com>
2021-09-03 14:01:36 +0100

    enhanced tracking

--- src/GATracking.js
+++ src/GATracking.js
@@ -1,5 +1,5 @@
-//import { useEffect, useState } from "react";
-//import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import ReactGA from 'react-ga';
 //import auth from './auth.ts'; // Sample authentication provider
 
@@ -31,8 +31,21 @@ import ReactGA from 'react-ga';
 export default GATracking; */
 
 function GATracking() {
-  ReactGA.initialize("UA-206607078-1", { testMode: process.env.NODE_ENV === 'test' });
-  ReactGA.pageview(window.location.pathname);
+        let location = useLocation();
+    const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+        if (!window.location.href.includes("localhost")) {
+    ReactGA.initialize("UA-206607078-1", { testMode: process.env.NODE_ENV === 'test' });
+            }
+        setInitialized(true);
+    }, []);
+
+    useEffect(() => {
+        if (initialized) {
+        ReactGA.pageview(location.pathname + location.search);
+        }
+    }, [initialized, location]);
 }
 
 export default GATracking;
