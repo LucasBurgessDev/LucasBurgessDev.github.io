61a9a532926add0572b2c69e44a82d0e2af89f5c
LucasBurgessDev <lucasburgess8@gmail.com>
2021-09-03 16:32:44 +0100

    adjusted if statement on tracking

--- src/GATracking.js
+++ src/GATracking.js
@@ -7,7 +7,7 @@ function GATracking() {
     const [initialized, setInitialized] = useState(false);
 
     useEffect(() => {
-        if (!window.location.href.includes("localhost")) {
+        if (!window.location.href.includes("lucasburgess")) {
             ReactGA.initialize("UA-206607078-1", { testMode: process.env.NODE_ENV === 'test' });
 //https://tacomanator.medium.com/environments-with-create-react-app-7b645312c09d
 //The value of NODE_ENV is set automatically to development (when using npm start), test (when using npm test) or production (when using npm build).
