db7b94ab1013914d6e7ccf113aa69c8170638503
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2022-05-01 19:39:57 +0100

    aligning test branch from main (#43)
    * Bump minimist from 1.2.5 to 1.2.6
    
    Bumps [minimist](https://github.com/substack/minimist) from 1.2.5 to 1.2.6.
    - [Release notes](https://github.com/substack/minimist/releases)
    - [Commits](https://github.com/substack/minimist/compare/1.2.5...1.2.6)
    
    ---
    updated-dependencies:
    - dependency-name: minimist
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump nanoid from 3.1.23 to 3.3.2
    
    Bumps [nanoid](https://github.com/ai/nanoid) from 3.1.23 to 3.3.2.
    - [Release notes](https://github.com/ai/nanoid/releases)
    - [Changelog](https://github.com/ai/nanoid/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/ai/nanoid/compare/3.1.23...3.3.2)
    
    ---
    updated-dependencies:
    - dependency-name: nanoid
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump async from 2.6.3 to 2.6.4
    
    Bumps [async](https://github.com/caolan/async) from 2.6.3 to 2.6.4.
    - [Release notes](https://github.com/caolan/async/releases)
    - [Changelog](https://github.com/caolan/async/blob/v2.6.4/CHANGELOG.md)
    - [Commits](https://github.com/caolan/async/compare/v2.6.3...v2.6.4)
    
    ---
    updated-dependencies:
    - dependency-name: async
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Forward and back buttons functionality added
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Luke Burgess <lukeburgess@rfu.com>

--- src/components/NavigationButtons.js
+++ src/components/NavigationButtons.js
@@ -1,25 +1,28 @@
-import { UseHistory } from "react-router-dom";
-import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-const [locationKeys, setLocationKeys] = useState([]);
-const history = useHistory();
+const Comp = () => {
+  const [locationKeys, setLocationKeys] = useState([]);
+  const history = useHistory();
 
-useEffect(() => {
-  return history.listen((location) => {
-    if (history.action === "PUSH") {
-      setLocationKeys([location.key]);
-    }
-
-    if (history.action === "POP") {
-      if (locationKeys[1] === location.key) {
-        setLocationKeys(([_, ...keys]) => keys);
-
-        // Handle forward event
-      } else {
-        setLocationKeys((keys) => [location.key, ...keys]);
-
-        // Handle back event
+  useEffect(() => {
+    return history.listen((location) => {
+      if (history.action === "PUSH") {
+        setLocationKeys([location.key]);
       }
-    }
-  });
-}, [locationKeys]);
+
+      if (history.action === "POP") {
+        if (locationKeys[1] === location.key) {
+          setLocationKeys(([_, ...keys]) => keys);
+
+          // Handle forward event
+        } else {
+          setLocationKeys((keys) => [location.key, ...keys]);
+
+          // Handle back event
+        }
+      }
+    });
+  }, [locationKeys]);
+
+  //...
+};
