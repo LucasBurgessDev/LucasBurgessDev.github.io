f6b54442c31cb275ee6215ba759b422efcc2ec8b
LucasBurgessDev <contact@lucasburgess.dev>
2023-02-06 19:09:29 +0000

    changes to navbar for resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,48 +25,55 @@ function NavBar() {
     window.addEventListener('resize', showButton);
 
     return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" OnClick=
-                    {closeMobileMenu}>
-                        <i className="fas fa-database"></i>LucasBurgessDev
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
-                                Projects
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
+      <>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo" OnClick={closeMobileMenu}>
+              <i className="fas fa-database"></i>LucasBurgessDev
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/projects"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Projects
+                </Link>
+              </li>
+              {/*                        <li className='nav-item'>
                             <Link to='/resume' className='nav-links' onClick={closeMobileMenu}>
                                 Resume
                             </Link>
                         </li>
-{/*                         <li className='nav-item'>
+                         <li className='nav-item'>
                             <Link to='/extra-projects' className='nav-links' onClick={closeMobileMenu}>
                                 Extra Projects
                             </Link>
                         </li> */}
-                        <li className='nav-item'>
-                            <Link to='/blog' className='nav-links' onClick={closeMobileMenu}>
-                                Blog
-                            </Link>
-                        </li>
-                    </ul>
-                    {button && <Button buttonStyle='btn--outline'>CONTACT ME</Button>}
-                </div>
-            </nav>
-        </>
-    )
+              <li className="nav-item">
+                <Link
+                  to="/blog"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Blog
+                </Link>
+              </li>
+            </ul>
+            {button && <Button buttonStyle="btn--outline">CONTACT ME</Button>}
+          </div>
+        </nav>
+      </>
+    );
 }
 
 export default NavBar
