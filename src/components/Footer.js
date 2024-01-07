ad1d22b3b41d0d21096fdfca4c1b3d755eda90b7
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2024-01-07 19:52:45 +0000

    Footer modifications, social links and button function switching

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,104 +1,86 @@
-/*import ReactDOM from 'react-dom';
-import { SocialIcon } from 'react-social-icons';
-                    <SocialIcon url="https://twitter.com/LukeyBurge" style={{ height: 50, width: 50 }} />
-                    <SocialIcon url="https://github.com/LucasBurgessDev" style={{ height: 50, width: 50 }} />
-                    <SocialIcon url="https://www.linkedin.com/in/luke-burgess-4b0975101/" style={{ height: 50, width: 50 }} /> */
-
-
 import React from 'react';
 import './Footer.css';
 import { Button } from './button';
-import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
           Join the newsletter to receive updates on new blog and project posts
         </p>
-        <p className='footer-subscription-text'>
+        <p className="footer-subscription-text">
           You can unsubscribe at any time.
         </p>
-        <div className='input-areas'>
+        <div className="input-areas">
           <form>
             <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
+              className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your Email"
             />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
+            <Button buttonStyle="btn--outline" >Subscribe</Button>
           </form>
         </div>
       </section>
-      <div class='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+      {/* <div class="footer-links">
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
             <h2>About Me</h2>
-            <Link to='/sign-up'>How it works</Link>
+            <Link to="/sign-up">How it works</Link>
           </div>
-          <div class='footer-link-items'>
+          <div class="footer-link-items">
             <h2>Contact Me</h2>
-            <Link to='/'>Contact</Link>
+            <Link to="/">Contact</Link>
           </div>
         </div>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
             <h2>Videos</h2>
-            <Link to='/'>Demos</Link>
+            <Link to="/">Demos</Link>
           </div>
-          <div class='footer-link-items'>
+          <div class="footer-link-items">
             <h2>Social Media</h2>
-            <Link to='/'>GitHub</Link>
-            <Link to='/'>LinkedIn</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
+            <Link to="/">GitHub</Link>
+            <Link to="/">LinkedIn</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
           </div>
         </div>
-      </div>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
-              <i class='fas fa-database'/>
-              LucasBurgessDev
-            </Link>
-          </div>
-          <small class='website-rights'>LucasBurgessDev</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link GitHub'
-              to='/'
-              target='_blank'
-              aria-label='GitHub'
+      </div> */}
+      <section class="social-media">
+        <div class="social-media-wrap">
+          <small class="website-rights">LucasBurgessDev</small>
+          <div class="social-icons">
+            <a
+              class="social-icon-link GitHub"
+              href="https://github.com/LucasBurgessDev"
+              aria-label="GitHub"
             >
-              <i class='fab fa-github' />
-            </Link>
-            <Link
-              class='social-icon-link LinkedIn'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
+              <i class="fab fa-github" />
+            </a>
+            <a
+              class="social-icon-link LinkedIn"
+              href="https://linkedin.com/luke-burgess-data"
+              aria-label="LinkedIn"
             >
-              <i class='fab fa-linkedin' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
+              <i class="fab fa-linkedin" />
+            </a>
+            <a
+              class="social-icon-link youtube"
+              href="https://youtube.com/@lucasburgessdev"
+              aria-label="Youtube"
             >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
+              <i class="fab fa-youtube" />
+            </a>
+            <a
+              class="social-icon-link twitter"
+              href="https://twitter.com/lucasburgessdev"
+              aria-label="Twitter"
             >
-              <i class='fab fa-twitter' />
-            </Link>
+              <i class="fab fa-twitter" />
+            </a>
           </div>
         </div>
       </section>
--- src/components/button.js
+++ src/components/button.js
@@ -11,23 +11,51 @@ export const Button = ({
         type, 
         onClick, 
         buttonStyle, 
-        buttonSize
+        buttonSize,
+        id
     }) => {
         const checkButtonStyle = STYLES.includes(buttonStyle) 
             ? buttonStyle 
             : STYLES(0);
 
             const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
-
-            return (
-                <Link to='/contactme' className='btn-mobile'>
-                    <button
+            if (id === "contactme") {
+              return (
+                <Link to="/contactme" className="btn-mobile">
+                  <button
                     className={`btn ${checkButtonStyle} ${checkButtonSize}`}
                     onClick={onClick}
                     type={type}
-                    >
-                        {children}
-                    </button>
+                  >
+                    {children}
+                  </button>
                 </Link>
-            )
+              );
+            }
+            else if (id === "demo") {
+                return (
+                <Link to="/projects" className="btn-mobile">
+                  <button
+                    className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+                    onClick={onClick}
+                    type={type}
+                  >
+                    {children}
+                  </button>
+                </Link>
+                );
+            }
+            else {
+              return (
+                <Link to="/projects" className="btn-mobile">
+                  <button
+                    className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+                    onClick={onClick}
+                    type={type}
+                  >
+                    {children}
+                  </button>
+                </Link>
+              );
+            }
 };
