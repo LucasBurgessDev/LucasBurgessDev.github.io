97c9fbdf40289bd5aecba158faae92938ef36484
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2024-02-11 21:18:43 +0000

    GA WIP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import NavBar from './components/common/NavBar';
+import React, { useEffect } from "react";
+import "./App.css";
+import NavBar from "./components/common/NavBar";
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,51 +10,44 @@ import {
 } from "react-router-dom";
 
 // Import the GA4 library (gtag.js)
-import { GA_MEASUREMENT_ID } from './config'; // Assuming you have your Measurement ID in a separate config file
+const GA_MEASUREMENT_ID = "G-DSTGL7K9W7";
 
-function App() {
-  // Initialize GA4
+// Custom hook for tracking page views
+function usePageViews() {
+  const location = useLocation();
+
   useEffect(() => {
     // Check if the environment is 'test' and return if true
-    if (process.env.NODE_ENV === 'test') {
+    if (process.env.NODE_ENV === "test") {
       return;
     }
 
-    // Initialize GA4 with your Measurement ID
-    window.gtag('config', GA_MEASUREMENT_ID);
-  }, []);
+    // Send pageview event on route change
+    window.gtag("config", GA_MEASUREMENT_ID, {
+      page_path: location.pathname,
+    });
+  }, [location]);
+}
 
-  // Send pageview events on route change
-  const trackPageview = () => {
-    const location = useLocation();
-    useEffect(() => {
-      // Send pageview event on route change
-      // Check if the environment is 'test' and return if true
-      if (process.env.NODE_ENV === 'test') {
-        return;
-      }
-
-      window.gtag('config', GA_MEASUREMENT_ID, {
-        page_path: location.pathname,
-      });
-    }, [location]);
-  };
+function App() {
+  // Call the custom hook to track page views
+  usePageViews();
 
   return (
     <>
       <Router>
         <NavBar />
         <Switch>
-          <Route path="/" exact component={() => { trackPageview(); return <Home />; }} />
-          <Route path="/projects" exact component={() => { trackPageview(); return <Projects />; }} />
-          <Route path="/resume" exact component={() => { trackPageview(); return <Resume />; }} />
-          <Route path="/contactme" exact component={() => { trackPageview(); return <ContactMe />; }} />
-          <Route path="/blog" exact component={() => { trackPageview(); return <Blog />; }} />
-          <Route path="/blog/:id" exact component={() => { trackPageview(); return <BlogPostPage />; }} />
+          <Route path="/" exact component={Home} />
+          <Route path="/projects" exact component={Projects} />
+          <Route path="/resume" exact component={Resume} />
+          <Route path="/contactme" exact component={ContactMe} />
+          <Route path="/blog" exact component={Blog} />
+          <Route path="/blog/:id" exact component={BlogPostPage} />
           {/* <Route path="/404" component={Error} /> */}
           <Redirect to="/404" component={Error} />
         </Switch>
-        <Footer/>
+        <Footer />
       </Router>
     </>
   );
