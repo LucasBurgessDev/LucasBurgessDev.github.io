5d3212d45786b6ea5fd575328d31d5116f95e157
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2024-02-11 23:19:26 +0000

    update tests to include reactDOM

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 test('renders app page', () => {
-  render(<App />);
-
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
 });
