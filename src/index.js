7f7b6d63966b523e25f60a0ac87bd77dca84a921
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2024-02-11 10:55:04 +0000

    Update index.js to remove GA3 tracking

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GATracking from './GATracking';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +14,4 @@ ReactDOM.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-GATracking();
 reportWebVitals();
