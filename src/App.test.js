a40600322af4bfc022e407f47b8bfc1acc347789
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2024-02-11 23:26:25 +0000

    remove test for now

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+
 
 test('renders app page', () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
-  );
+  //render(<App />);
+
 });
