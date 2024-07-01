84a39561e4e17a7b47547c3c84a4c3ec361997e3
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2024-07-01 22:43:46 +0100

    updates to fetch blogs from db

--- src/components/blog/BlogPage.js
+++ src/components/blog/BlogPage.js
@@ -1,34 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./BlogPage.css";
 import BlogList from "./BlogList.js";
 import EmptyList from "../common/EmptyList.js";
 import SearchBar from "../common/SearchBar.js";
-import { blogList as rawBlogList } from "../../config/data.js";
-//import { blogList } from "../../config/get_data.js";
-import lukeHeadshot from "../../images/Luke Headshot.jpg";
-import lukeDeckChair from "../../images/Luke Deck chair Twickenham Pic.jpeg";
-
-// Image mapping
-const imageMap = {
-  lukeHeadshot,
-  lukeDeckChair,
-};
-
-// Map the blogList to include actual image paths
-const blogList = rawBlogList.map(blog => ({
-  ...blog,
-  authorAvatar: imageMap[blog.authorAvatar], // Map the identifier to the actual image
-  cover: imageMap[blog.cover], // Map the identifier to the actual image
-}));
+//import { blogList as rawBlogList } from "../../config/data.js";
+//import lukeHeadshot from "../../images/Luke Headshot.jpg";
+//import lukeDeckChair from "../../images/Luke Deck chair Twickenham Pic.jpeg";
 
 const BlogPage = () => {
-  const [blogs, setBlogs] = useState(blogList);
+  const [blogs, setBlogs] = useState([]);
   const [searchKey, setSearchKey] = useState("");
+  const apiUrl = "https://get-blog-info-7hptrwqgna-nw.a.run.app";
+
+  // Define fetchBlogs function
+  const fetchBlogs = async () => {
+    try {
+      const response = await axios.get(apiUrl);
+      setBlogs(response.data);
+    } catch (error) {
+      console.error("Error fetching blog data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs(); // Fetch blogs when component mounts
+  }, []); // Empty dependency array ensures this effect runs once
 
   // Handle search key change
   const handleSearchKey = (searchKey) => {
     setSearchKey(searchKey);
-    const filteredBlogs = blogList.filter((blog) =>
+    const filteredBlogs = blogs.filter((blog) =>
       blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
     );
     setBlogs(filteredBlogs);
@@ -36,15 +38,14 @@ const BlogPage = () => {
 
   // Clear search and show all blogs
   const handleClearSearch = () => {
-    setBlogs(blogList);
     setSearchKey("");
+    fetchBlogs(); // Re-fetch all blogs
   };
 
   return (
     <div className="blog_page">
       <div className="page_name">Articles</div>
       <div className="search_bar">
-        {/* Search Bar */}
         <SearchBar
           value={searchKey}
           clearSearch={handleClearSearch}
@@ -53,7 +54,6 @@ const BlogPage = () => {
       </div>
       <div className="blog_post_container">
         <div className="blog_posts">
-          {/* Render Blog List or No Results Message */}
           {blogs.length ? (
             <BlogList blogs={blogs} />
           ) : (
