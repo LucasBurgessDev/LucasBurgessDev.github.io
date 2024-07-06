831fc51b3f3b6e0ef9a747bbd051d7dd7dffb8bc
LucasBurgessDev <62183529+LucasBurgessDev@users.noreply.github.com>
2024-07-06 01:23:20 +0100

    Removed react-pdf from project

--- src/components/resume/ResumeItems.js
+++ src/components/resume/ResumeItems.js
@@ -1,13 +1,6 @@
 import "../../App.css";
 import './Resume.css';
 import React from 'react';
-// using CommonJS modules
-import { Document, Page } from "react-pdf";
-//import pdfFile from "../docs/Burgess_Luke_Data_Architect.pdf";
-//import Video from '../videos/video-5.mp4';
-import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
 
 function ResumeItems() {
 
@@ -15,7 +8,7 @@ return (
         <div className='resume'>
 {/*             <video src={Video} alt="background - Video by Pressmaster from Pexels" autoPlay loop muted />*/}
             <div className="resume-pages">
-                         <Document
+{/*                          <Document
                             //file={pdfFile}
                         >
                             <Page pageNumber={1} />
@@ -24,7 +17,7 @@ return (
                             //file={pdfFile}
                         >
                             <Page pageNumber={2} />
-                        </Document>
+                        </Document> */}
             </div>
         </div>
     )
